d1b4e8fd159d08ce7910dcb8d8dfa2780e3f3890
onokajames96 <jamesonoka996@gmail.com>
2023-08-04 10:33:01 -0700

    update changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,14 +13,14 @@ unsigned int binary_to_uint(const char *b)
 int i = 0;
 unsigned int k = 0;
 
-  if (b == NULL)
-    return 0;
+if (b == NULL)
+return (0);
 
-  while (b[i] == '0' || b[i] == '1')
-  {
-    k <<= 1;
-   k += b[i]-'0';
- i++;
- }
- return (k);
+while (b[i] == '0' || b[i] == '1')
+{
+k <<= 1;
+k += b[i]-'0';
+i++;
+}
+return (k);
 }

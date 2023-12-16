695c7635a23687555d63c42df803f5eee16a26c5
onokajames96 <jamesonoka996gmail.com>
2023-12-16 07:33:59 -0800

    search algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search -searches for a value in an array of integers.
+ * @array: array of integers
+ * @size: The size.
+ * @value:The value being searched.
+ *
+ * Return: The index of the  value in array,
+ *         or -1 if value is not present or array is NULL.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t j;
+	
+	if (array == NULL)
+		return (-1);
+	
+	for (j = 0; j < size; ++j)
+	{
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+		
+		if (array[j] == value)
+			return (j);
+	}
+	return (-1);
+}

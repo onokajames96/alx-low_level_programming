7ddb3a335696856c806cc4f971b960ec94fcff59
onokajames96 <jamesonoka996gmail.com>
2023-12-16 07:47:38 -0800

    linear serch

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,14 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t j;
-	
+
 	if (array == NULL)
 		return (-1);
-	
+
+
 	for (j = 0; j < size; ++j)
 	{
 		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
-		
 		if (array[j] == value)
 			return (j);
 	}
